4e24ee5947a3e7a6a3c9e0d3ec98648390a2f8b5
Andrew Shephard <andrewshephard20@gmail.com>
2023-03-09 23:53:23 -0500

    fixed grid, add basic paint, working on buttons

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-const container = document.querySelector('#container');
+const grid = document.querySelector('.grid');
+let gridValue = document.querySelector('.grid-size');
+let gridSize = document.querySelector('input');
+const resetBtn = document.querySelector('.reset');
+const applyGridSize = document.querySelector('.apply');
+let squareSize = 8;
 
-for (i=0; i<16; i++){
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    grid.textContent = `${i}`;
-    
-    container.appendChild(grid);
+createGrid(squareSize);
+
+// Create Squared Divs
+function createDiv(size) {
+  const div = document.createElement('div');
+  div.classList.add('box');
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
+
+  return div;
 }
 
+// Creat The Grid and append it to grid
+function createGrid(gridSize) {
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      grid.appendChild(createDiv(grid.clientWidth / gridSize));
+    }
+  }
+  console.log(grid.clientWidth)
+}
 
+grid.addEventListener('mouseover', function(e){
+    if (e.target.matches('.box')) {
+        e.target.classList.add('filledIn');
+      }
+});
+
+function removeColor(e) {
+    e.target.classList.remove('filledIn');
+}
+
+function reset() {
+    const boxes = Array.from(document.querySelectorAll('div.box.filledIn'));
+    boxes.forEach(box => {
+        box.target.classList.remove('filledIn')
+    })
+    createGrid(squareSize);
+  }
+
+  resetBtn.addEventListener('click', reset);
