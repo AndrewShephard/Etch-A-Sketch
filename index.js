6df042774bb883d938eb14cbf5dfe6c2d442bfb8
Andrew Shephard <andrewshephard20@gmail.com>
2023-03-10 20:32:21 -0500

    added erase button with functionality

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const grid = document.querySelector('.grid');
 let gridValue = document.querySelector('.grid-size');
 let gridSize = document.querySelector('input');
 const resetBtn = document.querySelector('.reset');
+const eraseBtn = document.querySelector('.erase');
 const applyGridSize = document.querySelector('.apply');
 let squareSize = 8;
 
 createGrid(squareSize);
+draw();
 
 // Create Squared Divs
 function createDiv(size) {
@@ -27,17 +29,35 @@ function createGrid(gridSize) {
   console.log(grid.clientWidth)
 }
 
+function draw(){
 grid.addEventListener('mouseover', function(e){
+    if (e.target.matches('.box.erase')) {
+      e.target.classList.remove('erase');
+    }
     if (e.target.matches('.box')) {
         e.target.classList.add('filledIn');
       }
 });
+}
 
+function erase(){
+grid.addEventListener('mouseover', function(e){
+  if (e.target.matches('.box.filledIn')){
+    e.target.classList.remove('filledIn');
+  }
+  if (e.target.matches('.box')) {
+      e.target.classList.add('erase');
+    }
+});
+}
 
 
 function reset() {
     const boxes = Array.from(document.querySelectorAll('div.box.filledIn'));
     boxes.forEach(box => box.style.backgroundColor = "#f5f5f5");
+    draw();
 }
 
   resetBtn.addEventListener('click', reset);
+  eraseBtn.addEventListener('click', erase);
+  applyGridSize.addEventListener('click', draw);
